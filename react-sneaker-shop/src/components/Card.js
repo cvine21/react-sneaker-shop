df6fb9c045a482290d7ab730823890772f37fbda
Claudine Vine <rafarakh@gmail.com>
2022-08-28 13:13:38 +0300

    refactor: JSX code divided into components

---
+++ react-sneaker-shop/src/components/Card.js
@@ -0,0 +1,22 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="favourite">
+        <img src="/img/unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button">
+          <img width={11} height={11} src="/img/plus.svg" alt="plus"></img>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
