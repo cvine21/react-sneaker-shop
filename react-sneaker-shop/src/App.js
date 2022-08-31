766ceaba68d131b778385e66b11df8ef78d89a0a
Claudine Vine <rafarakh@gmail.com>
2022-08-31 11:46:12 +0300

    feat: items are added to the cart

--- react-sneaker-shop/src/App.js
+++ react-sneaker-shop/src/App.js
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
 function App() {
-    const [items, setItems] = React.useState([]);
-    const [cartOpened, setCartOpened] = React.useState(false);
+	const [items, setItems] = useState([]);
+	const [cartItems, setCartItems] = useState([]);
+	const [cartOpened, setCartOpened] = useState(false);
 
-    fetch("https://630c890c53a833c5342de89a.mockapi.io/items")
-        .then((res) => res.json())
-        .then((json) => setItems(json));
+	useEffect(() => {
+		fetch("https://630c890c53a833c5342de89a.mockapi.io/items")
+			.then((res) => res.json())
+			.then((json) => setItems(json));
+	}, []);
 
-    return (
-        <div className="wrapper clear">
-            {cartOpened && <Drawer onClickClose={() => setCartOpened(false)} />}
-            <Header onClickCart={() => setCartOpened(true)} />
-            <div className="content p-40">
-                <div className="d-flex align-center justify-between mb-40">
-                    <h1>Все кроссовки</h1>
-                    <div className="search-block">
-                        <img src="/img/search.svg" alt="Search"></img>
-                        <input placeholder="Поиск..." />
-                    </div>
-                </div>
+	return (
+		<div className="wrapper clear">
+			{cartOpened && (
+				<Drawer
+					items={cartItems}
+					onClickClose={() => setCartOpened(false)}
+				/>
+			)}
+			<Header onClickCart={() => setCartOpened(true)} />
+			<div className="content p-40">
+				<div className="d-flex align-center justify-between mb-40">
+					<h1>Все кроссовки</h1>
+					<div className="search-block">
+						<img src="/img/search.svg" alt="Search"></img>
+						<input placeholder="Поиск..." />
+					</div>
+				</div>
 
-                <div className="d-flex flex-wrap">
-                    {items.map((obj) => (
-                        <Card
-                            title={obj.title}
-                            price={obj.price}
-                            imageUrl={obj.imageUrl}
-                            onClickFavourite={() =>
-                                console.log("Добавили в закладки")
-                            }
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+				<div className="d-flex flex-wrap">
+					{items.map((item) => (
+						<Card
+							title={item.title}
+							price={item.price}
+							imageUrl={item.imageUrl}
+							onClickFavourite={() =>
+								console.log("Добавили в закладки")
+							}
+							onPlus={(obj) =>
+								setCartItems((prev) => [...prev, obj])
+							}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- react-sneaker-shop/src/components/Card/index.js
+++ react-sneaker-shop/src/components/Card/index.js
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Card.module.scss";
 
-function Card(props) {
-    const [isAdded, setIsAdded] = React.useState();
+function Card({ title, price, imageUrl, onClickFavourite, onPlus }) {
+	const [isAdded, setIsAdded] = useState();
 
-    const onClickPlus = () => {
-        setIsAdded(!isAdded);
-    };
+	const onClickPlus = () => {
+		onPlus({ title, imageUrl, price });
+		setIsAdded(!isAdded);
+	};
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.favourite} onClick={props.onClickFavourite}>
-                <img src="/img/unliked.svg" alt="Unliked" />
-            </div>
-            <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-            <h5>{props.title}</h5>
-            <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column">
-                    <span>Цена:</span>
-                    <b>{props.price} руб.</b>
-                </div>
-                <img
-                    className={styles.plus}
-                    src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
-                    alt="plus"
-                    onClick={onClickPlus}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.card}>
+			<div className={styles.favourite} onClick={onClickFavourite}>
+				<img src="/img/unliked.svg" alt="Unliked" />
+			</div>
+			<img width={133} height={112} src={imageUrl} alt="Sneakers" />
+			<h5>{title}</h5>
+			<div className="d-flex justify-between align-center">
+				<div className="d-flex flex-column">
+					<span>Цена:</span>
+					<b>{price} руб.</b>
+				</div>
+				<img
+					className={styles.plus}
+					src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+					alt="plus"
+					onClick={onClickPlus}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Card;
